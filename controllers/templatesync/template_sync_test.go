352cb098b35ed141836ee42f5bd800b589fa3234
Yi Rae Kim <yikim@redhat.com>
2024-03-20 17:39:34 -0400

    Fix template sync set to Nomcompliant when context cancel
    Bug: If the template-sync is shutting down and it's in the middle of a reconcile request, it can set the policy to noncompliant with this
    Ref: https://issues.redhat.com/browse/ACM-10402
    Signed-off-by: Yi Rae Kim <yikim@redhat.com>

--- controllers/templatesync/template_sync_test.go
+++ controllers/templatesync/template_sync_test.go
@@ -4,17 +4,27 @@ package templatesync
 
 import (
 	"context"
+	"errors"
+	"path/filepath"
 	"testing"
+	"time"
 
 	gktemplatesv1 "github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1"
+	corev1 "k8s.io/api/core/v1"
+	extensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/dynamic/fake"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/record"
 	configpoliciesv1 "open-cluster-management.io/config-policy-controller/api/v1"
 	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
+	client "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 func TestHandleSyncSuccessNoDoubleRemoveStatus(t *testing.T) {
@@ -211,5 +221,145 @@ func TestHasDuplicateNames(t *testing.T) {
 	has = hasDupName(&policy)
 	if !has { // expect duplicate detection to return true
 		t.Fatal("Duplicate name not detected")
+	}
+}
+
+func TestContextCancel(t *testing.T) {
+	configPolicy := configpoliciesv1.ConfigurationPolicy{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigurationPolicy",
+			APIVersion: "policy.open-cluster-management.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-configpolicy",
+			Namespace: "managed",
+		},
+		Status: configpoliciesv1.ConfigurationPolicyStatus{
+			ComplianceState: "",
+		},
+	}
+
+	outBytes, err := runtime.Encode(unstructured.UnstructuredJSONScheme, &configPolicy)
+	if err != nil {
+		t.Fatalf("Failed to encode ConfigurationPolicy: %s", err)
+	}
+
+	policy := &policiesv1.Policy{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Policy",
+			APIVersion: "policy.open-cluster-management.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-policy",
+			Namespace: "managed",
+		},
+		Spec: policiesv1.PolicySpec{
+			PolicyTemplates: []*policiesv1.PolicyTemplate{
+				{ObjectDefinition: runtime.RawExtension{Raw: outBytes}},
+			},
+		},
+		Status: policiesv1.PolicyStatus{
+			Details: []*policiesv1.DetailsPerTemplate{
+				{
+					ComplianceState: "NonCompliant",
+					History: []policiesv1.ComplianceHistory{
+						{
+							Message: "template-error; some error",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "managed"}}
+
+	scheme := runtime.NewScheme()
+
+	err = corev1.AddToScheme(scheme)
+	if err != nil {
+		t.Fatalf("Failed to set up the scheme: %s", err)
+	}
+
+	err = policiesv1.AddToScheme(scheme)
+	if err != nil {
+		t.Fatalf("Failed to set up the scheme: %s", err)
+	}
+
+	err = extensionsv1.AddToScheme(scheme)
+	if err != nil {
+		t.Fatalf("Failed to set up the scheme: %s", err)
+	}
+
+	err = configpoliciesv1.AddToScheme(scheme)
+	if err != nil {
+		t.Fatalf("Failed to set up the scheme: %s", err)
+	}
+
+	recorder := record.NewFakeRecorder(10)
+
+	testEnv := &envtest.Environment{
+		CRDDirectoryPaths: []string{filepath.Join("..", "..", "config", "test", "crd")},
+		Scheme:            scheme,
+	}
+	defer func() {
+		_ = testEnv.Stop()
+	}()
+
+	config, err := testEnv.Start()
+	if err != nil {
+		t.Fatalf("Failed to start testEnv")
+	}
+
+	fakeClient, err := client.New(config, client.Options{Scheme: scheme})
+	if err != nil {
+		t.Fatalf("Failed to create the Client")
+	}
+
+	err = fakeClient.Create(context.TODO(), ns)
+	if err != nil {
+		t.Fatalf("Failed to create the Namespace")
+	}
+
+	err = fakeClient.Create(context.TODO(), policy)
+	if err != nil {
+		t.Fatalf("Failed to convert the Policy")
+	}
+
+	err = fakeClient.Create(context.TODO(), &configPolicy)
+	if err != nil {
+		t.Fatalf("Failed to convert the ConfigPolicy")
+	}
+
+	fakeClientset := kubernetes.NewForConfigOrDie(config)
+
+	reconciler := PolicyReconciler{
+		Recorder:  recorder,
+		Client:    fakeClient,
+		Clientset: fakeClientset,
+		Config:    config,
+	}
+
+	// Sleep time
+	tests := []time.Duration{200, 100, 50, 10, time.Nanosecond * 10}
+
+	for _, sleepTime := range tests {
+		ctx, cancelFunc := context.WithCancel(context.Background())
+
+		go func() {
+			time.Sleep(sleepTime)
+			cancelFunc()
+		}()
+
+		input := reconcile.Request{NamespacedName: types.NamespacedName{Name: "test-policy", Namespace: "managed"}}
+
+		_, err = reconciler.Reconcile(ctx, input)
+		if !errors.Is(err, context.Canceled) {
+			t.Fatal("Should be the context canceled error")
+		}
+
+		if len(recorder.Events) != 0 {
+			t.Fatal("Should not emit the canceled error")
+		}
 	}
 }
